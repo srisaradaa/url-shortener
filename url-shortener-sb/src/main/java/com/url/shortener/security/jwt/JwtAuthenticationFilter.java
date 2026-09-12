9c9a078acdb10764e90f3ad82795a31a417d6321
srisaradaa <srisaradaa069@gmail.com>
2026-09-12 11:03:22 +0530

    JWT Authentication added

--- url-shortener-sb/src/main/java/com/url/shortener/security/jwt/JwtAuthenticationFilter.java
+++ url-shortener-sb/src/main/java/com/url/shortener/security/jwt/JwtAuthenticationFilter.java
@@ -1,4 +1,4 @@
-package com.url.shortener.security;
+package com.url.shortener.security.jwt;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
--- url-shortener-sb/src/main/java/com/url/shortener/security/jwt/JwtAuthenticationResponse.java
+++ url-shortener-sb/src/main/java/com/url/shortener/security/jwt/JwtAuthenticationResponse.java
@@ -1,4 +1,4 @@
-package com.url.shortener.security;
+package com.url.shortener.security.jwt;
 
 import lombok.Data;
 
